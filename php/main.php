27cabf6c04ab5189531c5633d3337e4c6089aee7
Quick00 <dammusicgames@gmail.com>
2019-02-25 09:18:20 +0100

    Added php folder

---
+++ php/main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BusinessTracker</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/main.css">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> -->
+
+
+</head>
+<body onscroll="OnScroll()">
+    <header class="bovenkant" >
+            <nav>
+                <div class="scroll" id="scroll">
+                    
+                    <ul>
+                        <a href="#" ><li id="color1">BusinessTracker</li></a>
+                        <a href="#"><li id="color2">About the Team</li></a>
+                        <a href="#"><li id="color3">Contact</li></a>
+                    </ul>
+
+                   <a href="#"> <button type="button" name="loginbtn" class="loginbtn" id="loginbtn">Login</button></a>
+                    
+                </div> 
+            </nav>
+        
+            <h3 class="bg-text">BusinessTracker</h3>
+          
+        
+        
+        
+        
+
+    </header>
+
+    <div class="midden">
+
+       
+
+    </div>
+
+    <div class="onderkant">
+
+    </div>
+
+
+
+    <script src="../js/main.js"></script>
+</body>
+</html>
