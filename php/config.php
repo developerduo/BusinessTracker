a7477dec780d955c0563843bee660d5fe9be06c6
Jurrie Piek <jurrie39@gmail.com>
2019-02-26 12:27:42 +0100

    Updated Config

--- php/config.php
+++ php/config.php
@@ -6,7 +6,11 @@ $password = '';
 $db = 'businesstracker';
 
 try{
-    $conn = new PDO("mysql:host=$host;dbname=dbnaam", $username, $password);
-    
-
+    $conn = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    if($conn) {
+    echo "Connected successfully";
+    }
+} 
+catch(PDOException $E) {
+    echo $E->getMessage();
 }
