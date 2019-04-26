db5c43ecb217936ce32d6e3e469585c957627819
Jurrie Piek <jurrie39@gmail.com>
2019-04-26 11:30:11 +0100

    DIngen

--- public/app/php/controlpanel.php
+++ public/app/php/controlpanel.php
@@ -15,9 +15,18 @@ if(!isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../resources/css/controlpanel.css">
     <title>Control Panel</title>
 </head>
 <body>
-    
+    <nav class='navBar'>
+        <h2>Control<br>Panel</h2>
+        <ul class='list'>
+            <li class='listKop'>Gebruikers</li>
+            <li class='listKop'>Inplannen</li>
+            <li class='listKop'>Uitbetalen</li>
+            <li class='listKop'>Instellingen</li>
+        </ul>
+    </nav>
 </body>
 </html>
